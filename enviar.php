2ccd9de539ed35092fcfed39b8b53225ccbffd39
Xavier Gaspata <xaviergaspa123@gmail.com>
2021-02-28 09:10:38 -0500

    codigo para enviar mensaje desde el formulario

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php 
+    $destino = "xaviergaspa123@gmail.com";
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+    header("Location:gracias.html");
+
+?>
